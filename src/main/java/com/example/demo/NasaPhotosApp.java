4811d7ffa00afd7a3458650ec12605790b1ab6e4
willschleicher <schleicher.will@gmail.com>
2025-04-15 22:05:59 -0500

    Initial commit Spring Initializer

---
+++ src/main/java/com/example/demo/NasaPhotosApp.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaPhotosApp
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NasaPhotosApp.class, args);
+    }
+
+}
